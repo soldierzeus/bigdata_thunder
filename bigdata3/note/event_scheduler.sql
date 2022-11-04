f68954b5583270debf57bccf7fcd7a03170e814e
soldierzeus <hjsnam@naver.com>
2022-11-04 22:19:55 +0900

    update data_sql

--- bigdata3/note/event_scheduler.sql
+++ bigdata3/note/event_scheduler.sql
@@ -1,10 +1,8 @@
-SHOW VARIABLES LIKE 'event%';
-SET GLOBAL event_scheduler = ON;
-CREATE EVENT IF NOT EXISTS 'event%'
-    ON SCHEDULE
-       AT '2022-11-04 18:23:00'
-    ON COMPLETION NOT PRESERVE
-    ENABLE
-    COMMENT '완료'
-    DO
-END
+CREATE TABLE newspaperdb.mail_economytable (
+ seq        INT NOT NULL AUTO_INCREMENT,
+ mb_id     VARCHAR(20),
+ mb_pw    VARCHAR(20),
+ address   VARCHAR(50),
+ mb_tell    VARCHAR(50),  
+  PRIMARY KEY(seq)
+) ENGINE=MYISAM CHARSET=utf8mb3;
